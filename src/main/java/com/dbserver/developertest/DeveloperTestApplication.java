d760ef311ffd01e8740805a0fc72db6356ae41c2
pedroandrades <pehfass@gmail.com>
2020-01-07 21:16:53 -0300

    Spring Project Created

---
+++ src/main/java/com/dbserver/developertest/DeveloperTestApplication.java
@@ -0,0 +1,13 @@
+package com.dbserver.developertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeveloperTestApplication.class, args);
+    }
+
+}
